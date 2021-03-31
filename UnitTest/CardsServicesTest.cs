3fac956300234d138126783a3e5fea75e6c0c301
Anthea <d0509327@mail.fcu.edu.tw>
2021-03-31 16:12:08 +0800

    Refactor unit test

--- UnitTest/CardsServicesTest.cs
+++ UnitTest/CardsServicesTest.cs
@@ -12,20 +12,34 @@ namespace UnitTest
     public class CardsServicesTest
     {
         private CardsService _cardsService;
-        private CardsController _cardsController;
         
         [SetUp]
         public void Setup()
         {
             _cardsService = Substitute.For<CardsService>();
-            _cardsController = new CardsController(_cardsService);
         }
 
         [Test]
         public void When_Accounting_Cards_Empty_Should_Create_Default_Card()
         {
-            _cardsController.Index();
-            _cardsService.Received().CreateDefaultCard(new List<Card>());
+            var card = GivenEmptyCardList();
+            CreateDefaultCardInEmptyCardList(card);
+            CardListShouldHasOneCardAfterCreateDefaultCard(card.Count);
+        }
+
+        private static void CardListShouldHasOneCardAfterCreateDefaultCard(int cardCount)
+        {
+            Assert.AreEqual(1, cardCount);
+        }
+
+        private void CreateDefaultCardInEmptyCardList(List<Card> emptyCard)
+        {
+            _cardsService.CreateDefaultCard(emptyCard);
+        }
+
+        private List<Card> GivenEmptyCardList()
+        {
+            return new List<Card>();
         }
     }
 }
